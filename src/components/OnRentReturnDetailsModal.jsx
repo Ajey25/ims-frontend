01d6c66dd4bece5bd9a7d234b3e9d769d98a21f5
Ajey25 <ajay.prajapati1630as@example.com>
2025-04-24 12:00:21 +0530

    25th commit

--- src/components/OnRentReturnDetailsModal.jsx
+++ src/components/OnRentReturnDetailsModal.jsx
@@ -209,7 +209,9 @@ const OnRentReturnDetailsModal = ({ orr, onClose }) => {
                         textDecoration: "underline",
                       }}
                       onClick={() => {
-                        const url = `http://localhost:5173/app/transactions/onrent?onRentNo=${getOnRentNo(
+                        const url = `${
+                          import.meta.env.VITE_API_BASE_URL2
+                        }/app/transactions/onrent?onRentNo=${getOnRentNo(
                           item.onRentNo
                         )}`;
                         window.open(url, "_blank");
